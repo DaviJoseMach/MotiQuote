91351e64a207380f5efc8b9202986b0996285146
DaviJoseMach <thech.nasa.dj@gmail.com>
2024-08-11 22:32:47 -0300

    alterando landing page

--- src/home.js
+++ src/home.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { fetchQuote } from './quoteService';
 import './App.css';
+import './Collaborators.css';
+import collaboratorsData from './collaborators.json';
 
 const Home = () => {
   const [quote, setQuote] = useState('');
@@ -36,6 +38,28 @@ const Home = () => {
     window.open(twitterUrl, '_blank', 'noopener,noreferrer');
   };
 
+  useEffect(() => {
+    const counters = document.querySelectorAll('.counter-number');
+  
+    counters.forEach(counter => {
+      const updateCounter = () => {
+        const target = +counter.getAttribute('data-target');
+        const current = +counter.innerText;
+
+        const increment = target / 400; 
+
+        if (current < target) {
+          counter.innerText = Math.ceil(current + increment);
+          setTimeout(updateCounter, 20);
+        } else {
+          counter.innerText = target;
+        }
+      };
+
+      updateCounter();
+    });
+  }, []); 
+
   return (
     <div className="Home">
       <div className="navbar">
@@ -61,13 +85,61 @@ const Home = () => {
               <i className="fa-solid fa-share-nodes"></i> 
               <span> Share</span>
           </button>
-
         </div>
         <a className="button-icon-w" href="https://www.pixme.bio/davvzin">
           <i className="fa-solid fa-mug-hot icon-b"></i>
           <span className="button-text">Buy me a drink</span>
-        </a>
+        </a> <br></br>
       </div>
+      <div className="counters-container">
+        <div className="counter-card">
+          <div className="counter">
+            <span className="plus-symbol">+</span>
+            <span className="counter-number" data-target="6000">0</span>
+          </div>
+          <h4>Quotes Generated</h4>
+        </div>
+        <div className="counter-card">
+          <div className="counter">
+            <span className="plus-symbol">+</span>
+            <span className="counter-number" data-target="56">0</span>
+          </div>
+          <h4>Motivated people</h4>
+        </div>
+        <div className="counter-card">
+          <div className="counter">
+            <span className="plus-symbol"></span>
+            <span className="counter-number" data-target="1852">0</span>
+          </div>
+          <h4>Authors</h4>
+        </div>
+        <div className="counter-card">
+          <div className="counter">
+            <span className="plus-symbol">+</span>
+            <span className="counter-number" data-target="3065">0</span>
+          </div>
+          <h4>Motivated mornings</h4>
+        </div>
+      </div>
+      
+      <center>
+          <h1>Collaborators</h1>
+          <p className='infus'>To appear here, just send a donation of just <b className='zum'>0.1</b> dollars or reais to the project</p> 
+      </center>
+      <div className='collaborators'>
+        <div className="collaborators-track">
+          {collaboratorsData.map((collaborator) => (
+            <div key={collaborator.id} className={`collaborator ${collaborator.deviceType}`}>
+              <img
+                src={collaborator.imageUrl}
+                alt={collaborator.name}
+                className="collaborator-photo"
+              />
+            </div>
+          ))}
+        </div>
+      </div> 
+      <br></br><br></br>
     </div>
   );
 };
